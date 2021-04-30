88463b282a0863066f74e4aa325f914880c059ee
macktrain <82425068+macktrain@users.noreply.github.com>
2021-04-29 22:31:06 -0600

    updated json

--- assets/js/portfolioXML.js
+++ assets/js/portfolioXML.js
@@ -1,33 +1,10 @@
-function buildHTML(jsonArr) 
-{
-    var html = "";
-    var i;
-    for(i = 0; i < jsonArr.length; i++) 
-    {
-        html += "<div class='workDiv'>";
-        html += "     <div class='exampleName'>";
-        html += "          " +   jsonArr[i].exName;
-        html += "     </div>";
-        html += "     <div class='description'>";
-        html += "          " +   jsonArr[i].description;
-        html += "     </div>";
-        html += "     <div class='techUsed'>";
-        html += "          " +   jsonArr[i].techUsed;
-        html += "     </div>";
-        html += "     <div class='url'>";
-        html += "          " +   jsonArr[i].url;
-        html += "     </div>";
-        html += "</div>";
-    }
-    document.getElementById("work").innerHTML = html;
-    console.log(html);
-}
-
 var url = "https://macktrain.github.io/sandlot-week2/assets/json/portfolio.json";
 
 var xmlhttp = new XMLHttpRequest();
+
 xmlhttp.onreadystatechange = function() 
 {
+    
     if (this.readyState == 4 && this.status == 200) 
     {
         var portfolioArr = JSON.parse(this.responseText);
@@ -37,3 +14,30 @@ xmlhttp.onreadystatechange = function()
 
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
+
+function buildHTML(x) 
+{
+    var html = "";
+    var i;
+    
+    for(i = 0; i < x.length; i++) 
+    {
+        alert (x.example[i].exName);
+        html += "   <div class='workDiv'>";
+        html += "       <div class='exampleName'>";
+        html += "           " +   x[i].exName;
+        html += "        </div>";
+        html += "       <div class='description'>";
+        html += "           " +   x[i].description;
+        html += "       </div>";
+        html += "       <div class='techUsed'>";
+        html += "           " +   x[i].techUsed;
+        html += "       </div>";
+        html += "       <div class='url'>";
+        html += "           " +   x[i].url;
+        html += "       </div>";
+        html += "   </div>";
+    }
+    document.getElementById("workHTML").innerHTML = html;
+    alert ("*"+ html + "*");
+}
